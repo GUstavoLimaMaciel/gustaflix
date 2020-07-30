2ef0957e646884643508042deca47d884d51cc0f
suub-dev-master <suub@suub.com.br>
2020-07-29 23:12:22 -0300

    Carrousel works

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import CadastroCategoria from './pages/cadastro/Categoria';
 import CadastroVideo from './pages/cadastro/Video';
