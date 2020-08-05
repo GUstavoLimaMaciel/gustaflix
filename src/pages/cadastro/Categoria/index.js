5e5939a35828c9eef03c16a625260d5aa9959c06
suub-dev-master <suub@suub.com.br>
2020-08-04 23:24:58 -0300

    New form

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
 import FormField from '../../../components/FormField';
+import Button from '../../../components/Button';
 
 function CadastroCategoria() {
   const [categorias, setCategoria] = useState([]);
@@ -9,61 +10,75 @@ function CadastroCategoria() {
   const valoresIniciais = {
     nome: '',
     descricao: '',
-    cor: '#000000'
+    cor: '#000000',
   };
 
   const [novaCategoria, setnovaCategoria] = useState(valoresIniciais);
 
-  function changeCategory(event){
+  function changeCategory(event) {
     setnovaCategoria({
       ...novaCategoria,
-      [event.target.getAttribute('name')]: event.target.value
+      [event.target.getAttribute('name')]: event.target.value,
     });
   }
 
-  function saveCategory(event){
+  function saveCategory(event) {
     event.preventDefault();
-    if(novaCategoria){
+    if (novaCategoria) {
       setCategoria([
-        ...categorias, 
-        novaCategoria
+        ...categorias,
+        novaCategoria,
       ]);
       setnovaCategoria(valoresIniciais);
     }
   }
-  
+
+  useEffect(() => {
+    if (window.location.href.includes('localhost')) {
+      const URL = 'http://localhost:3000/categorias';
+      fetch(URL)
+        .then(async (respostaDoServer) => {
+          if (respostaDoServer.ok) {
+            const resposta = await respostaDoServer.json();
+            setCategoria(resposta);
+            return;
+          }
+          throw new Error('Não foi possível pegar os dados');
+        });
+    }
+  }, []);
+
   return (
     <PageDefault>
-      <h1>Cadastro de Categoria: {novaCategoria.nome}</h1>
+      <h1>
+        Cadastro de Categoria:
+        {novaCategoria.nome}
+      </h1>
 
       <form onSubmit={saveCategory}>
 
-        <FormField value={novaCategoria.nome} onChange={changeCategory} name="nome" type="text" label="Nome da Categoria"/>
-        <FormField value={novaCategoria.descricao} onChange={changeCategory} name="descricao" type="textarea" label="Descrição da Categoria"/>
-        <FormField value={novaCategoria.cor} onChange={changeCategory} name="cor" type="color" label="Cor da Categoria"/>
+        <FormField value={novaCategoria.nome} onChange={changeCategory} name="nome" type="text" label="Nome da Categoria" />
+        <FormField value={novaCategoria.descricao} onChange={changeCategory} name="descricao" type="textarea" label="Descrição da Categoria" />
+        <FormField value={novaCategoria.cor} onChange={changeCategory} name="cor" type="color" label="Cor da Categoria" />
 
-        <button>
+        <Button>
           Cadastrar
-        </button>
+        </Button>
       </form>
 
-
       <ul>
-        {categorias.map((categoria, index) => {
-          return (
-            <li key={index}>
-              {categoria.nome}
-            </li>
-          )
-        })}
+        {categorias.map((categoria, index) => (
+          <li key={index}>
+            {categoria.nome}
+          </li>
+        ))}
       </ul>
-
 
       <Link to="/">
         Ir para home
       </Link>
     </PageDefault>
-  )
+  );
 }
 
 export default CadastroCategoria;
